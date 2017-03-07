2e417f5fa97a9d385ce12a79195bf5376d72a4c2
Joan Pau Castells <joanpau.castells@hotmail.com>
2017-03-07 12:51:31 +0100

    Application Advanced

--- sobres/isobres/views.py
+++ sobres/isobres/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def mainpage(request):
+    page= """
+    <html>
+    <body>
+    Sobres, a PP aplication.
+    </body>
+    </html>
+    """
+    return HttpResponse(page)
